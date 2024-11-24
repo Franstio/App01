305218de2843dbcd3de3cb6dc531bdd41bf173a2
Franstio <franstiodiego01@gmail.com>
2024-11-24 23:39:54 +0700

    Update

--- App01/Form2.Designer.cs
+++ App01/Form2.Designer.cs
@@ -1,14 +1,14 @@
 ﻿namespace App01
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,49 +23,36 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             richTextBox1 = new RichTextBox();
-            button1 = new Button();
             SuspendLayout();
             // 
             // richTextBox1
             // 
-            richTextBox1.Anchor = AnchorStyles.Top;
-            richTextBox1.Location = new Point(183, 12);
+            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.Location = new Point(0, 0);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(385, 117);
+            richTextBox1.Size = new Size(800, 450);
             richTextBox1.TabIndex = 0;
             richTextBox1.Text = "";
             // 
-            // button1
-            // 
-            button1.Anchor = AnchorStyles.Top;
-            button1.Location = new Point(362, 152);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 1;
-            button1.Text = "PASS TO FORM2";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // Form1
+            // Form2
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
             Controls.Add(richTextBox1);
-            Name = "Form1";
-            Text = "Form1";
+            Name = "Form2";
+            Text = "Form2";
             ResumeLayout(false);
         }
 
         #endregion
 
         private RichTextBox richTextBox1;
-        private Button button1;
     }
 }
---
+++ App01/Teks.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App01
+{
+    public class Teks
+    {
+        public string teks { get; set; } = string.Empty;
+    }
+}
